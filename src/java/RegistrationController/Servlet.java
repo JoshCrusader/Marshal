6d3c565d688ee9ac99da8a1cfae7a51973b35c37
JoshCrusader <joshua_cruzada@dlsu.edu.ph>
2017-12-01 20:27:41 +0800

    yes po

--- src/java/RegistrationController/Servlet.java
+++ src/java/RegistrationController/Servlet.java
@@ -24,6 +24,7 @@ import dao.UserDAO;
 import javax.servlet.http.HttpSession;
 import model.Ref_Properties;
 import model.Users;
+import model.Vehicles;
 
 
 /**
@@ -41,8 +42,6 @@ public class Servlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
   
-    //String occupation; int bNo; int lNo; String street; int tNo; int mNo;
-    //String k1; String k2;String v1; String v2;String email;String pw;String pw2;
      String lName= request.getParameter("lName");
      String fName= request.getParameter("fName");
      String mName= request.getParameter("mName");
@@ -59,26 +58,29 @@ public class Servlet extends HttpServlet {
      int bNo= Integer.parseInt(request.getParameter("bNo"));
      int lNo= Integer.parseInt(request.getParameter("lNo"));
      int renting= Integer.parseInt(request.getParameter("rent"));
-     int tempOID = 0;
      boolean isexistingUname= false;
      boolean isvalidPw= false;
+     String movingIn = null;
+     String[] carmodels= request.getParameterValues("c1");
+     String[] pnum= request.getParameterValues("v1");
 
     
-        System.out.println("UserType: " + usertype);
+    System.out.println("UserType: " + usertype);
      
     try{
          Users existingUser= UserDAO.getUserbyUsername(uName);
-         Users existingHo= HomeownerDAO.GetHomeOwner(uName);
-         Ref_Properties existingBno= PropertyDAO.getProperties(lNo, bNo);
+         String existingHo= UserDAO.existingHomeowner(bNo, lNo);
+         System.out.print("Existing HomeOwner: " + existingHo);
+         Ref_Properties existingBno= UserDAO.getProperties(bNo, lNo);
         
         if (existingUser==null){
+            System.out.println("Success!");
             
         }
         else{
             System.out.println("Invalid");
             request.setAttribute("Existing","NO");      
         }
-        
         if (pw.equals(pw2)){
             isvalidPw= true;
         }
@@ -87,14 +89,8 @@ public class Servlet extends HttpServlet {
             System.out.println("Invalid");
             request.setAttribute("Invalid","NO");
         }
-        
-        Class.forName("com.mysql.jdbc.Driver"); //must be changed to DatabaseUtils for it to be shorter
-        Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","");
-
-       
-        
         if (existingBno==null){
-            System.out.println(existingBno);
+            System.out.println("Not existing");
             System.out.println("Invalid");
             request.setAttribute("existingBno","NO");
            
@@ -104,101 +100,27 @@ public class Servlet extends HttpServlet {
         }
         
         if ((isvalidPw==true) && (existingBno!=null)){
-
-              String occ= "Select occupationID from ref_occupation where occupation='"+occupation+"'";
-              PreparedStatement o= con.prepareStatement(occ);
-              ResultSet rs = o.executeQuery();
-              if (rs.next()){
-                  tempOID = rs.getInt(1);
-              }
-              else{
-                  String occInsert="Insert into ref_occupation (occupation) VALUES ('"+occupation+"')";
-                  PreparedStatement ins= con.prepareStatement(occInsert);
-                  ins.executeUpdate();
-                  String occ1= "Select occupationID from ref_occupation where occupation='"+occupation+"'";
-                  PreparedStatement o1= con.prepareStatement(occ1);
-                  ResultSet rs1 = o1.executeQuery();
-                  rs1.beforeFirst();
-                  System.out.print(occ1);
-                  tempOID = rs1.getInt(1);
-              }
-              
-              String[] carmodels= request.getParameterValues("c1");
-              String[] pnum= request.getParameterValues("v1");
-              System.out.println(carmodels.length);
+            int tempOID= UserDAO.existingOccupation(occupation);
+            UserDAO.registerUser(uName, pw, lName, fName, mName, bDate, usertype, tempOID, movingIn, tNum, mNum,  email);
+            UserDAO.insertPnum(uName, pnum, carmodels);
             
-              for(int i=0; i<pnum.length; i++){
-                   String model= "Insert into vehicles (platenum, model) VALUES ('"+pnum[i]+"','"+carmodels[i]+"')";
-                   PreparedStatement m= con.prepareStatement(model);
-                   System.out.println("model");
-                   m.executeUpdate();
-                   String userVehicle= "Insert into user_vehicles (platenum, userID) VALUES ('"+pnum[i]+"','"+uName+"')";
-                   PreparedStatement user= con.prepareStatement(userVehicle);
-                   System.out.println("userVehicle");
-                   //user.executeUpdate();
-              }
-            String statement= "INSERT INTO users  (userID, passwd, lname, fname, mame, bDate, usertypeID, occupationID, movingIn, telno,phoneno,email) VALUES ('"+uName+"','"+pw+"','"+lName+"','"+mName+"','"+fName+"','"+bDate+"', "+usertype+", '"+tempOID+"', NOW(), '"+tNum+"', '"+mNum+"','"+email+"')";
-            PreparedStatement a = con.prepareStatement(statement);
-            System.out.print(statement);
-            a.executeUpdate();
-            User a1= new User(uName);
-            a1.setPw(pw);
-            a1.setfName(lName); 
-            a1.setlName(fName);
-            a1.setmName(mName);
-            a1.setDate(bDate);  
-            a1.setUsertype(usertype);
-            a1.settNum(tNum);
-            a1.setmNum(mNum); 
-            a1.setEmail(email);
-            HttpSession session = request.getSession();
-            session.setAttribute("RegisterdUser",a1);
-           
-            /*String referenceproperties= "INSERT INTO ref_properties (blocknum,lotnum,mappointID) VALUES ("+bNo+","+lNo+",2)"; //need to insert other rows
-            PreparedStatement ref= con.prepareStatement(referenceproperties);
-            System.out.print(referenceproperties);
-            ref.executeUpdate();
-            request.setAttribute("user",a1);*/
-            
-            if (livingAs==1){ //homeowner
-
-              String sql= "INSERT INTO homeowner  (blocknum,lotnum,userid) VALUES ('"+bNo+"','"+lNo+"','"+uName+"')";
-              PreparedStatement ho = con.prepareStatement(sql);
-              System.out.print(sql);
-              ho.executeUpdate();
-              //Homeowner h1= new Homeowner(bNo,lNo);
-              //h1.setUserID(uName);
-              //request.setAttribute("homeowner",h1);
+            if (livingAs==1 && existingHo==null){ //homeowner
+              UserDAO.registerHomeowner(uName, bNo, lNo);
             }
-            else if (livingAs==2){
-              String sql= "INSERT INTO homemember  (userid,blocknum,lotnum, renting) VALUES ('"+uName+"',"+bNo+","+lNo+","+renting+")";
-              PreparedStatement ho = con.prepareStatement(sql);
-              System.out.print(sql);
-              ho.executeUpdate();
-              //Homemember h1= new Homemember(uName);
-              //h1.setbNo(bNo);
-              //h1.setlNo(lNo);
-              //request.setAttribute("homemember",h1);
+            else if (livingAs==2 && (existingHo!=null)){
+              UserDAO.registerHomemember(uName, bNo, lNo, renting);
             }
             else if ((livingAs==3) && (existingHo!=null)){
-              String sql= "INSERT INTO kasambahay (userid,blocknum,lotnum) VALUES ('"+uName+"',"+bNo+","+lNo+")";
-              PreparedStatement ho = con.prepareStatement(sql);
-              System.out.print(sql);
-              ho.executeUpdate();
-       
-              //Kasambahay h1 = new Kasambahay(uName);
-              //h1.setbNo(bNo);
-              //h1.setlNo(lNo);
-              //request.setAttribute("kasambahay",h1);
+              System.out.print("HEYAH");
+              UserDAO.registerKasambahay(uName, bNo, lNo);
             }
-            
+             //request.getRequestDispatcher("home.jsp").forward(request,response);
         }
           
         else{
              request.getRequestDispatcher("reg.jsp").forward(request,response);
         }
-          
-        con.close();
+         
     } catch (Exception e){
         System.out.println(e);
     }
--- src/java/dao/UserDAO.java
+++ src/java/dao/UserDAO.java
@@ -5,11 +5,19 @@
  */
 package dao;
 
+import Objects.User;
+import RegistrationController.Homemember;
+import RegistrationController.Homeowner;
+import RegistrationController.Kasambahay;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import javax.servlet.http.HttpSession;
 import model.DatabaseUtils;
+import model.Ref_Properties;
 import model.Users;
+import model.Vehicles;
 
 /**
  *
@@ -78,6 +86,250 @@ public class UserDAO {
             }
             return user;
     }
-    
-   
+    public static ArrayList<Homeowner>GetAllHomeOwner(){
+        Connection conn = DatabaseUtils.retrieveConnection();
+        ArrayList<Homeowner> owners = new ArrayList<Homeowner>();
+        try{
+            String sql = "SELECT * FROM USERS U RIGHT JOIN HOMEOWNER HO ON HO.userID = U.userID;";
+            PreparedStatement pStmt = conn.prepareStatement(sql);
+            ResultSet rs = pStmt.executeQuery();
+            while(rs.next()){
+                Homeowner ho = new Homeowner(rs.getString(1), rs.getString(2),rs.getInt(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getString(7),rs.getInt(8),rs.getInt(9),rs.getString(10),rs.getInt(11),rs.getInt(12),rs.getString(13),rs.getString(14),rs.getString(15),rs.getInt(16),rs.getInt(17));
+               owners.add(ho);
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        finally{
+            if(conn != null){
+                try{
+                        conn.close();
+                }catch(Exception e){}
+            }
+        }
+        return owners;
+        }
+      public static Homeowner GetHomeOwner(String userId){
+        Connection conn = DatabaseUtils.retrieveConnection();
+        Homeowner owner = null;
+        try{
+            String sql = "SELECT * FROM USERS U RIGHT JOIN HOMEOWNER HO ON HO.userID = U.userID where U.userID = '"+userId+"';";
+            PreparedStatement pStmt = conn.prepareStatement(sql);
+            ResultSet rs = pStmt.executeQuery();
+            System.out.print(rs);
+            while(rs.next()){
+                owner = new Homeowner(rs.getString(1), rs.getString(2),rs.getInt(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getString(7),rs.getInt(8),rs.getInt(9),rs.getString(10),rs.getInt(11),rs.getInt(12),rs.getString(13),rs.getString(14),rs.getString(15),rs.getInt(16),rs.getInt(17));
+                
+            } 
+       }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        finally{
+            if(conn != null){
+                try{
+                        conn.close();
+                }catch(Exception e){}
+            }
+        }
+        return owner;
+    }
+     public static void registerUser(String userID, String passwd, String lname, String fname, String mame, String bDate, int usertypeID, int occupationID, String movingIn, String telno, String phoneno, String email){
+           
+            String statement= "INSERT INTO users  (userID, passwd, lname, fname, mame, bDate, usertypeID, occupationID, movingIn, telno,phoneno,email) VALUES (?,?,?,?,?,?,?,?, NOW(),?,?,?)";
+            Connection conn = DatabaseUtils.retrieveConnection();
+            try{
+                
+                PreparedStatement a = conn.prepareStatement(statement);
+                System.out.print(statement);
+                    a.setString(1, userID);
+                    a.setString(2, passwd);
+                    a.setString(3, lname);
+                    a.setString(4, fname);
+                    a.setString(5,mame);
+                    a.setString(6, bDate);
+                    a.setInt(7, usertypeID);
+                    a.setInt(8, occupationID);
+                    a.setString(9, telno);
+                    a.setString(10, phoneno);
+                    a.setString(11, email);
+                    a.executeUpdate();
+            }catch(Exception e){
+                    e.printStackTrace();
+            }finally{
+                    if(conn != null){
+                            try{
+                                    conn.close();
+                            }catch(Exception e){}
+                    }
+            }
+    }
+      public static Ref_Properties getProperties(int blocknum, int lotnum){
+        Connection conn = DatabaseUtils.retrieveConnection();
+        Ref_Properties property = null;
+        try{
+            String sql = "SELECT * FROM REF_PROPERTIES WHERE BLOCKNUM = "+blocknum+" AND LOTNUM = "+lotnum+";";
+            PreparedStatement pStmt = conn.prepareStatement(sql);
+            ResultSet rs = pStmt.executeQuery();
+            while(rs.next()){
+                property = new Ref_Properties(rs.getInt(1),rs.getInt(2),rs.getByte(3),rs.getString(4),rs.getInt(5),rs.getInt(6));
+                
+            } 
+       }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        finally{
+            if(conn != null){
+                try{
+                        conn.close();
+                }catch(Exception e){}
+            }
+        }
+        return property;
+    }
+      public static void registerHomeowner( String userID, int blocknum, int lotnum){
+     
+       String statement=  "INSERT INTO homeowner  (userID,blocknum,lotnum) VALUES ('"+userID+"','"+blocknum+"','"+lotnum+"')";
+       Connection conn = DatabaseUtils.retrieveConnection();
+       try{
+             PreparedStatement ho = conn.prepareStatement(statement);
+             System.out.print(statement);
+             ho.executeUpdate();
+       }catch(Exception e){
+               e.printStackTrace();
+       }finally{
+               if(conn != null){
+                       try{
+                               conn.close();
+                       }catch(Exception e){}
+               }
+       }
+       
+    }
+      
+      public static void registerHomemember(String userID, int blocknum, int lotnum, int renting){
+       Homemember user = null;
+       String statement=  "INSERT INTO homemember (userID, blocknum, lotnum, renting) VALUES ('"+userID+"','"+blocknum+"','"+lotnum+"','"+renting+"')";
+       Connection conn = DatabaseUtils.retrieveConnection();
+       try{
+             PreparedStatement ho = conn.prepareStatement(statement);
+             System.out.print(statement);
+             ho.executeUpdate();
+       }catch(Exception e){
+               e.printStackTrace();
+       }finally{
+               if(conn != null){
+                       try{
+                               conn.close();
+                       }catch(Exception e){}
+               }
+       }
+     
+    }
+       public static void registerKasambahay(String userID,int blocknum, int lotnum){
+       Kasambahay user = null;
+       String sql= "INSERT INTO kasambahay (userID,blocknum,lotnum) VALUES ('"+userID+"',"+blocknum+","+lotnum+")";
+       Connection conn = DatabaseUtils.retrieveConnection();
+       try{
+             PreparedStatement ho = conn.prepareStatement(sql);
+             System.out.print(sql);
+             ho.executeUpdate();
+       }catch(Exception e){
+               e.printStackTrace();
+       }finally{
+               if(conn != null){
+                       try{
+                               conn.close();
+                       }catch(Exception e){}
+               }
+       }
+    }
+    public static void insertPnum(String userID, String[] platenum, String[] carmodels){
+        Vehicles pnum= null;
+        Connection con = DatabaseUtils.retrieveConnection();
+        try{
+         for(int i=0; i<platenum.length; i++){
+                   
+                   String model= "Insert into vehicles (platenum, model) VALUES ('"+platenum[i]+"','"+carmodels[i]+"')";
+                   PreparedStatement m= con.prepareStatement(model);
+                   System.out.println("model");
+                   m.executeUpdate();
+                   String userVehicle= "Insert into user_vehicles (platenum, userID) VALUES ('"+platenum[i]+"','"+userID+"')";
+                   PreparedStatement user= con.prepareStatement(userVehicle);
+                   System.out.println("userVehicle");
+                   user.executeUpdate();
+              }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }finally{
+                    if(con != null){
+                            try{
+                                    con.close();
+                            }catch(Exception e){}
+                }
+         }
+    }
+     public static int existingOccupation(String occupation){
+        int tempOID= 0;
+        String occ= "Select occupationID from ref_occupation where occupation='"+occupation+"'";
+        Connection con = DatabaseUtils.retrieveConnection();
+        try{
+              PreparedStatement o= con.prepareStatement(occ);
+              ResultSet rs = o.executeQuery();
+              if (rs.next()){
+                  tempOID = rs.getInt(1);
+              }
+              else{
+                  String occInsert="Insert into ref_occupation (occupation) VALUES ('"+occupation+"')";
+                  PreparedStatement ins= con.prepareStatement(occInsert);
+                  ins.executeUpdate();
+                  String occ1= "Select occupationID from ref_occupation where occupation='"+occupation+"'";
+                  PreparedStatement o1= con.prepareStatement(occ1);
+                  ResultSet rs1 = o1.executeQuery();
+                  rs1.beforeFirst();
+                  System.out.print(occ1);
+                  tempOID = rs1.getInt(1);
+              }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }finally{
+                    if(con != null){
+                            try{
+                                    con.close();
+                            }catch(Exception e){}
+                }
+         }
+        return tempOID;
+     }
+      public static String existingHomeowner(int blocknum, int lotnum){
+        Connection conn = DatabaseUtils.retrieveConnection();
+        String user = null;
+        String sql = "SELECT * FROM HOMEOWNER WHERE BLOCKNUM = "+blocknum+" AND LOTNUM = "+lotnum+";";
+        try{
+            PreparedStatement o= conn.prepareStatement(sql);
+              ResultSet rs = o.executeQuery();
+              if (rs.next()){
+                  user = rs.getString(1);
+              }
+              else{
+                  user= null;
+              }
+            } 
+       
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        finally{
+            if(conn != null){
+                try{
+                        conn.close();
+                }catch(Exception e){}
+            }
+        }
+        return user;
+    }
 }

a161ca63e1603af49d5f99d1459e09a02640e9dc
fredpurisimaIT <32771993+fredpurisimaIT@users.noreply.github.com>
2017-12-02 01:03:27 +0800

    Update Database.java

--- src/java/VehicleAdmin/dao/Database.java
+++ src/java/VehicleAdmin/dao/Database.java
@@ -19,7 +19,7 @@ public class Database {
     private static final String DRIVER_NAME ="com.mysql.jdbc.Driver";
     private static final String JDBC_URL ="jdbc:mysql://localhost:3306/mydb";
     private static final String DB_USER ="root";
-    private static final String DB_PASSWORD ="Kuroneko_1";
+    private static final String DB_PASSWORD ="";
                     
     /**It gets the database connection of MySQL
      *
